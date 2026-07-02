08efc6018d8d938a66d0f2e3417017e2a6d974fb
Ediz Yurdakul <edizyurdakul@gmail.com>
2026-07-02 20:21:39 +0300

    chore(tooling): install and configure husky, commitlint and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "content",
+      ],
+    ],
+    "subject-case": [0],
+  },
+};
+
+export default config;
